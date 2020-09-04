13feaf740e367a6c78e4fd9ef9f16403cfd2c02e
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-04 15:46:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,4 @@ class Song
     @@all
   end
   
-  def songs
-    self.all.select {|song| song.name == self}
-  end
-  
-  def artists
-    self.all.collect {|artist| artist.name == self}
-  end
-  
 end

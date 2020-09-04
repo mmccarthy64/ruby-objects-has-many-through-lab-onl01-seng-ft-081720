78f35bc9d865d5f84790e4edf2da5ff0313e2692
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-04 15:45:26 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,27 @@
+class Genre
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name)
+    @@all << song
+  end
+  
+  def songs
+    self.all.select {|song| song.name == self}
+  end
+  
+  def artists
+    self.all.collect {|genre| genre.name == self}
+  end
+  
+end
